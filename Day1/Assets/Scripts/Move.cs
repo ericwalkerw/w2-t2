5a3a571909b77c391d102c0dd951502155c95484
ericwalkerw <ericwalker.leo@gmail.com>
2023-07-19 13:48:58 +0700

    move_gameobject

---
+++ Day1/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform[] destination;
+    public float speed = 5f;
+
+    private int point = 0;
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, destination[point].position) < 0.1f)
+        {
+            point = Random.Range(0, destination.Length);
+        }
+
+        MoveToPoint();
+    }
+
+    public void MoveToPoint()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination[point].position, speed * Time.deltaTime);
+    }
+
+
+}
